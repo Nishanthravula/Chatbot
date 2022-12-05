8c8b45f367e2bf8e456f5b8966343cf28e265be4
PM-Harshith <harshithpokalamuni@gmail.com>
2022-12-05 18:34:02 -0500

    added topic search

--- chatbot/chat_bot.py
+++ chatbot/chat_bot.py
@@ -1,126 +1,114 @@
-# # -*- coding: utf-8 -*-
-#
-#
-# import json
-# # if you are using python 3, you should
-# import urllib
-# import urllib.request
-# import urllib.parse
-# from googletrans import Translator
-#
-# translator = Translator()
-# model_name = 'BM25'
-#
-#
-# def translate_and_get_query(text):
-#     lang_code = translator.detect(text).lang
-#     if lang_code == 'en':
-#         text_en = text
-#         text_ru = translator.translate(text, src=lang_code, dest='ru').text
-#         text_de = translator.translate(text, src=lang_code, dest='de').text
-#     elif lang_code == 'de':
-#         text_de = text
-#         text_en = translator.translate(text, src=lang_code, dest='en').text
-#         text_ru = translator.translate(text, src=lang_code, dest='ru').text
-#     else:
-#         text_ru = text
-#         text_en = translator.translate(text, src=lang_code, dest='en').text
-#         text_de = translator.translate(text, src=lang_code, dest='de').text
-#     return [text_en, text_ru, text_de], lang_code
-#
-#
-# def parser():
-#     with open('test-queries.txt', encoding="utf-8") as f:
-#         index = 1
-#         for line in f.readlines():
-#             query = line.split(' ', 1)
-#             query[1] = query[1].strip()
-#             query[1] = query[1].replace(":", "")
-#             translated_query, lang_code = translate_and_get_query(query[1])
-#             if lang_code == 'en':
-#                 final_query = "text_en:(%s)^1.62 OR text_ru:(%s)^0.5 OR text_de:(%s)^0.8" % (
-#                     translated_query[0], translated_query[1]
-#                     , translated_query[2])
-#             elif lang_code == 'ru':
-#                 final_query = "text_en:(%s)^1.33 OR text_ru:(%s)^1.62 OR text_de:(%s)^0.4" % (
-#                     translated_query[0], translated_query[1]
-#                     , translated_query[2])
-#             else:
-#                 final_query = "text_en:(%s)^1.33 OR text_ru:(%s)^0.5 OR text_de:(%s)^1.62" % (
-#                     translated_query[0], translated_query[1]
-#                     , translated_query[2])
-#             print(final_query)
-#             dictionary = {"q": final_query}
-#             encoded_query = urllib.parse.urlencode(dictionary)
-#             print(encoded_query)
-#             qid = query[0]
-#             ir_model = model_name
-#             inurl = 'http://104.196.61.19:8983/solr/' + ir_model + '/select?' + encoded_query + '&fl=id%2Cscore&wt=json&indent=true&rows=20'
-#             print(inurl)
-#             data = urllib.request.urlopen(inurl)
-#             docs = json.load(data)['response']['docs']
-#             rank = 0
-#             outfn = model_name + '/%d.txt' % index
-#             outf = open(outfn, 'w')
-#             print(outfn)
-#             for doc in docs:
-#                 outf.write(qid + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(
-#                     doc['score']) + ' ' + ir_model + '\n')
-#                 rank += 1
-#             index += 1
-#         outf.close()
-#         f.close()
-#
-#
-# parser()
-
 '''
 @author: Harshith Pokala Muni
 Institute: University at Buffalo
 '''
 
-from tqdm import tqdm
 import json
 
 import urllib
 import urllib.request
 import urllib.parse
-import argparse
-from flask import Flask
 from flask import request
-import hashlib
+from similarity import classifier
+from flask import Flask
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 ir_model = 'mycol1'
+reddit_ir_model = 'reddit'
+similarity_classifier = classifier()
 
 
-
-@app.route("/bot", methods=['POST'])
-def execute_query():
-    """ This function handles the POST request to your endpoint.
-        Do NOT change it."""
-    topic_name = request.args.get('topic')
-    query = request.args.get('query')
+def get_top_5_chit_chat_query_matches(query):
     solr_query = "text:(%s)" % query
     query_dictionary = {"q": solr_query}
     encoded_solr_query = urllib.parse.urlencode(query_dictionary)
 
     solr_url = 'http://104.196.61.19:8983/solr/' + ir_model + '/select?' + encoded_solr_query + '&fl=message_id%2Ctext%2Cscore&wt=json&indent' \
-                                                                                     '=true&rows=1 '
+                                                                                                '=true&rows=5'
     print(solr_url)
     query_result = urllib.request.urlopen(solr_url)
 
-    query_docs = json.load(query_result)['response']['docs'][0]
-    message_id = query_docs['message_id'][0]
+    return json.load(query_result)['response']['docs']
 
-    solr_response_query = "parent_message_id:\"%s\"" % message_id
+
+def get_top_10_chit_chat_answers(solr_response_query):
+    # solr_response_query = "parent_message_id:\"%s\"" % message_id
     response_dictionary = {"q": solr_response_query}
     encoded_solr_response_query = urllib.parse.urlencode(response_dictionary)
-    solr_response_url = 'http://104.196.61.19:8983/solr/' + ir_model + '/select?' + encoded_solr_response_query + '&fl=message_id%2Ctext%2Cscore&wt=json&indent' \
-                                                                                                '=true&rows=1 '
+    solr_response_url = 'http://104.196.61.19:8983/solr/' + ir_model + '/select?' + encoded_solr_response_query + '&fl=text%2Cscore&wt=json&indent' \
+                                                                                                                  '=true&rows=10 '
     response_result = urllib.request.urlopen(solr_response_url)
-    response_docs = json.load(response_result)['response']['docs'][0]
-    return response_docs['text'][0]
+    return json.load(response_result)['response']['docs']
+
+
+def get_top_5_topic_query_matches(topic, query):
+    solr_query = "selftext:(%s)" % query
+    topic_query_dictionary = {"q": solr_query}
+    encoded_solr_query = urllib.parse.urlencode(topic_query_dictionary)
+    facet_query = urllib.parse.urlencode({"facet.query": "topic:\"%s\"" % topic})
+    solr_url = 'http://35.222.156.17:8983/solr/' + reddit_ir_model + '/select?' + encoded_solr_query + \
+               '&fl=id%2Cscore&wt=json&indent=true&rows=5&facet=true&' + \
+               facet_query
+    print(solr_url)
+    response_result = urllib.request.urlopen(solr_url)
+    return json.load(response_result)['response']['docs']
+
+
+def get_top_10_topic_answers(documents):
+    string = ""
+    for document in documents:
+        reddit_id = document['id']
+        if not string:
+            string += "parent_id:\"%s\"" % ("t3_" + reddit_id)
+        else:
+            string += " OR " + "parent_id:\"%s\"" % ("t3_" + reddit_id)
+
+    solr_query = {"q": string}
+    encoded_solr_query = urllib.parse.urlencode(solr_query)
+    solr_response_url = 'http://35.222.156.17:8983/solr/' + reddit_ir_model + '/select?' + encoded_solr_query + '&fl=body%2Cscore&wt=json&indent' \
+                                                                                                                '=true&rows=10 '
+    print(solr_response_url)
+    response_result = urllib.request.urlopen(solr_response_url)
+    return json.load(response_result)['response']['docs']
+
+
+def topic_query(topic, query):
+    topic_query_documents = get_top_5_topic_query_matches(topic, query)
+    documents = get_top_10_topic_answers(topic_query_documents)
+    result = [document['body'] for document in documents]
+    bot_reply = similarity_classifier.get_top_similarity_answer(query, result)
+    return bot_reply
+
+
+def chit_chat_query(query):
+    query_docs = get_top_5_chit_chat_query_matches(query)
+    string = ""
+    for doc in query_docs:
+        message_id = doc['message_id'][0]
+        string = string + "parent_message_id:\"%s\"" % message_id + " or "
+    response_docs = get_top_10_chit_chat_answers(string)
+    result = [document['text'][0] for document in response_docs]
+    bot_reply = similarity_classifier.get_top_similarity_answer(query, result)
+    return bot_reply
+
+
+@app.route("/bot", methods=['POST'])
+@cross_origin(origin='*')
+def execute_query():
+    """ This function handles the POST request to your endpoint.
+        Do NOT change it."""
+    request_json = dict(request.json)
+    topic_name = ""
+    print(request_json.keys)
+    if 'topic' in request_json.keys():
+        topic_name = request_json['topic']
+    query = request_json['query']
+    if not topic_name:
+        return chit_chat_query(query)
+    else:
+        return topic_query(topic_name, query)
+
 
 if __name__ == "__main__":
     """ Driver code for the project, which defines the global variables.
